aea3b3f63674c7021da5b1785ff5fb720fa0f9b4
Traman <tramgup@gmail.com>
2025-12-30 12:38:41 -0500

    preping for easy deployment

--- backend/app.py
+++ backend/app.py
@@ -123,5 +123,6 @@ cleanup_thread.start()
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5050)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port)
     
